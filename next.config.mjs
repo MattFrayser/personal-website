0fd7f9e5331b93408929f95605f86897e4cd902f
Matt Frayser <frayserm@vcu.edu>
2025-02-18 14:56:24 -0500

    pages fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   basePath: "/personal-website",
+   //basePath: "/personal-website",
    //output: "export",
-   reactStrictMode: true,
+   //reactStrictMode: true,
 };
 
 export default nextConfig;
